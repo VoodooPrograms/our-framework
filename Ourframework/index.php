25bcbe65cf72218aee46ce4c962ab87d674b864c
VoodooPrograms <arkanidius@gmail.com>
2019-11-15 21:35:07 +0100

    Init commit with file structure

--- Ourframework/index.php
+++ Ourframework/index.php
@@ -1,12 +1,1 @@
 <?php
-
-declare(strict_types=1);
-
-spl_autoload_register( function ($path) {
-   if (preg_match('/\\\\/', $path)) {
-       $path = str_replace('\\', DIRECTORY_SEPARATOR, $path);
-   }
-   if (file_exists(get_include_path()."${path}.php")) {
-       require_once("${path}.php");
-   }
-});
