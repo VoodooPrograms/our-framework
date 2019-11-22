5460b04159ffc9b29855df007c5959aceaeba623
VoodooPrograms <arkanidius@gmail.com>
2019-11-22 16:27:11 +0100

    Blueprint of request class

--- Ourframework/Core/Request.php
+++ Ourframework/Core/Request.php
@@ -4,7 +4,42 @@
 namespace Ourframework\Core;
 
 
-class Request
+abstract class Request
 {
+    protected $path = "/";
+    protected $properties = [];
+    protected $status = 0;
 
+    final public function __construct(){
+        $this->launch();
+    }
+
+    abstract protected function launch();
+
+    public function getProperty(string $key): string {
+        if (!isset($this->properties[$key])){
+            return null;
+        }
+        return $this->properties[$key];
+    }
+
+    public function setProperty(string $key, $val): void {
+        $this->properties[$key] = $val;
+    }
+
+    public function getPath(): string {
+        return $this->path;
+    }
+
+    public function setPath(string $path): void {
+        $this->path = $path;
+    }
+
+    public function getStatus(): int {
+        return $this->status;
+    }
+
+    public function setStatus(int $status): void {
+        $this->status = $status;
+    }
 }
