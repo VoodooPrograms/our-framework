9630368fc96821f3aedb8f07a2643a0ef38b5ff6
VoodooPrograms <arkanidius@gmail.com>
2020-02-13 13:23:58 +0100

    Adding render to BlogController

--- Ourframework/User/Controllers/BlogController.php
+++ Ourframework/User/Controllers/BlogController.php
@@ -13,6 +13,9 @@ class BlogController extends Controller
     public function index(Request $request): int
     {
         echo "BlogController";
-        return 1;
+
+        $a1='a2';
+        return $this->render('User/Templates/test4.php', ['apple_key' => "apple_value", 'a1_key' => '$a1_value',
+            "tablica" => ["Aleks1_key" => "Ola1_value", "Aleks2_key" => "Ola2_value"]]);
     }
 }
