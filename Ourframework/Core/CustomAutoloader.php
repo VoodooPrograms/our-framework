a375a484329088f0335eff8dfc85c91ffc0b5aff
VoodooPrograms <arkanidius@gmail.com>
2020-03-15 13:45:23 +0100

    Moving autoloader functionality to special class, preparing for customLoaders

---
+++ Ourframework/Core/CustomAutoloader.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Ourframework\Core;
+
+
+class CustomAutoloader extends Autoloader
+{
+    final public function autoloadCustom($path){
+
+    }
+}
--- Ourframework/index.php
+++ Ourframework/index.php
@@ -6,18 +6,23 @@ declare(strict_types=1);
  * These part will be changed to Autoload class, maybe Composer autoload.
  * For now everyone should create class in namespaces that reflect directory structure
  */
-set_include_path(dirname(getcwd()).DIRECTORY_SEPARATOR);
+//set_include_path(dirname(getcwd()).DIRECTORY_SEPARATOR);
 
-require_once("vendor/autoload.php");
+//require_once("vendor/autoload.php");
+//
+//spl_autoload_extensions(".php");
+//
+//spl_autoload_register(function ($path) {
+//    if (preg_match('/\\\\/', $path)) {
+//        $path = str_replace('\\', DIRECTORY_SEPARATOR, $path);
+//    }
+//    if (file_exists(get_include_path()."${path}.php")) {
+//        require_once("${path}.php");
+//    }
+//});
 
-spl_autoload_register(function ($path) {
-    if (preg_match('/\\\\/', $path)) {
-        $path = str_replace('\\', DIRECTORY_SEPARATOR, $path);
-    }
-    if (file_exists(get_include_path()."${path}.php")) {
-        require_once("${path}.php");
-    }
-});
+require_once ("Core/Autoloader.php");
+$loader = new \Ourframework\Core\Autoloader();
 
 use Ourframework\Core\App;
 
